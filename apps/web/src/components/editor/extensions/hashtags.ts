7ea1cd6f95d62bdc024afcc0504691e0d9820854
Nokdu <thinkong@hotmail.com>
2026-01-30 16:56:16 +0000

    feat(13-02): implement hashtag extension for CodeMirror
    - Added hashtagHighlighter using MatchDecorator
    - Added hashtagAutocomplete CompletionSource
    - Added hashtagTheme for visual feedback

---
+++ apps/web/src/components/editor/extensions/hashtags.ts
@@ -0,0 +1,74 @@
+import {
+  MatchDecorator,
+  Decoration,
+  ViewPlugin,
+  DecorationSet,
+  EditorView,
+  ViewUpdate,
+} from "@codemirror/view";
+import {
+  autocompletion,
+  CompletionContext,
+  CompletionResult,
+} from "@codemirror/autocomplete";
+
+const hashtagDecoration = Decoration.mark({
+  class: "cm-hashtag",
+});
+
+const hashtagMatcher = new MatchDecorator({
+  regexp: /#[\w-]+/g,
+  decoration: () => hashtagDecoration,
+});
+
+export const hashtagHighlighter = ViewPlugin.fromClass(
+  class {
+    hashtags: DecorationSet;
+    constructor(view: EditorView) {
+      this.hashtags = hashtagMatcher.createDeco(view);
+    }
+    update(update: ViewUpdate) {
+      this.hashtags = hashtagMatcher.updateDeco(update, this.hashtags);
+    }
+  },
+  {
+    decorations: (instance) => instance.hashtags,
+  },
+);
+
+export const hashtagTheme = EditorView.baseTheme({
+  ".cm-hashtag": {
+    color: "#88c0d0", // nordfrost1
+    fontWeight: "bold",
+  },
+});
+
+export function hashtagAutocomplete(getTags: () => string[]) {
+  return autocompletion({
+    override: [
+      async (context: CompletionContext): Promise<CompletionResult | null> => {
+        const word = context.matchBefore(/#[\w-]*/);
+        if (!word) return null;
+        if (word.from === word.to && !context.explicit) return null;
+
+        const tags = getTags();
+        const options = tags.map((tag) => ({
+          label: tag.startsWith("#") ? tag : `#${tag}`,
+          type: "keyword",
+        }));
+
+        return {
+          from: word.from,
+          options,
+          validFor: /^#[\w-]*$/,
+        };
+      },
+    ],
+  });
+}
+
+export const hashtagExtensions = (getTags: () => string[]) => [
+  hashtagHighlighter,
+  hashtagTheme,
+  hashtagAutocomplete(getTags),
+];
